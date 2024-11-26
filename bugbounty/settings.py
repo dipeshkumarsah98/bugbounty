e8eb512dc74fffecd1ef885def806adfa99826dd
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2024-11-26 22:30:10 +0545

    fix: update vercel config to include static files

--- bugbounty/settings.py
+++ bugbounty/settings.py
@@ -140,6 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This is where collectstatic will gather files
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
