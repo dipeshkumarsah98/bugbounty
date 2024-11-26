0a35d0119811164aaeeac8cc87f0bc0ad144a6c7
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2024-11-26 21:32:03 +0545

    fix: update allowed host

--- bugbounty/settings.py
+++ bugbounty/settings.py
@@ -18,16 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1", 
-    "vercel.app",
-    "bugbounty-ecru.vercel.app"
-    ]
-
-
-print('environment', os.environ.get('DB_PASSWORD'))
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app", ".now.sh"]
 
 # Application definition
 AUTH_USER_MODEL = 'api.User'  
