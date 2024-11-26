3c25800f9357f15ed38465591670c38833dfb2eb
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2024-11-26 23:05:50 +0545

    chore: update vercel script to test static files

--- bugbounty/settings.py
+++ bugbounty/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory where static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Directory where static files will be collected
 
 
 # Default primary key field type
